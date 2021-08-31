ead8789012855e3c3f86bb4f73d03231e6eec1df
quasi99 <eszquasar@gmail.com>
2021-08-31 15:02:15 -0300

    add fields to  Event table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_175718) do
+ActiveRecord::Schema.define(version: 2021_08_31_180014) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_08_31_175718) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
     t.string "location"
+    t.date "date"
+    t.time "time"
   end
 
   create_table "users", force: :cascade do |t|
