9343fbc0e0703dcd19d64c678c9a6dad7a52aca5
quasi99 <eszquasar@gmail.com>
2021-08-31 14:39:32 -0300

    create Attendee_Event db

---
+++ app/models/attendees_event.rb
@@ -0,0 +1,2 @@
+class AttendeesEvent < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_173247) do
+ActiveRecord::Schema.define(version: 2021_08_31_173724) do
+
+  create_table "attendees_events", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
