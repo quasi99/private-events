a9c9e23cc86df8073275a801b8aa36d23d2a2108
quasi99 <eszquasar@gmail.com>
2021-08-31 17:58:32 -0300

    config controllers

---
+++ app/controllers/attendees_events_controller.rb
@@ -0,0 +1,9 @@
+class AttendeesEventsController < ApplicationController
+	
+	private
+
+	def attendees_event_params
+		params.require(:attendees_event).permit(:event_id)	
+	end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show]
+
+	def show
+	end
+
+	def delete_user_image
+		@user_image = ActiveStorage::Attachment.find_by(id: current_user.image.id)
+		@user_image.purge_later
+		redirect_back(fallback_location: root_path)
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+end
