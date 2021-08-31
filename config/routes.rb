ecfdd931c10bd07cc57ea17096b31d69383d4ab5
quasi99 <eszquasar@gmail.com>
2021-08-31 15:19:37 -0300

    config changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+
+  resources :events do
+  	resources :attendees_events, only: [:new, :create, :destroy]
+  	member do
+  		get 'attend'
+  		get 'unattend'
+  	end
+  end
+
+  resources :users, only: [:show]
+
+  get 'users/edit/delete_user_image', to: 'users#delete_user_image'
+
   root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
