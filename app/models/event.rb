2a0f3e561f72dca348918ccfb0013b4edb76e992
quasi99 <eszquasar@gmail.com>
2021-08-31 18:39:15 -0300

    update models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+	scope :past, -> { where("date < ?", Date.today) }
+	scope :upcoming, -> { where("date >= ?", Date.today) }
+
+	belongs_to :creator, class_name: 'User'
+	has_many :attendees_events, dependent: :destroy
+	has_many :attendees, through: :attendees_events, dependent: :destroy
+	has_one_attached :image
+
+	validates :title, :descripition, :date, :time, :location, :image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
+  has_many :attendees_events, foreign_key: :atteendee_id, dependent: :destroy
+  has_many :ateended_events, through: :attendees_events, souce: :event, dependent: :destroy
+  has_one_attached :image
+  validate :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
